25cf96c5deee3f5b95a0aeab08dd1f87aaec2411
Alex Kalmuk <alexkalmuk@gmail.com>
2021-01-10 11:07:14 +0300

    Zero call_info struct

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "main.h"
 
@@ -7,6 +8,8 @@ struct demo_call_info *call_info;
 
 int main(int argc, char *argv[]) {
 	call_info = malloc(sizeof (struct demo_call_info));
+
+	memset(call_info, 0, sizeof *call_info);
 
 	demo_pj_main(argc, argv, demo_nk_main);
 
